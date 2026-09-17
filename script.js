31ca416032990d6db7db606f76c898c0a0dc8d39
ifther <aktharkhan556@gmail.com>
2026-09-18 00:31:54 +0530

    advance1

---
+++ script.js
@@ -0,0 +1,29 @@
+/* const a = document.querySelectorAll('.montly-amt')
+console.log(a.length);
+for (let i = 0; i < a.length; i++) {
+    a[i].classList.add('hide-plan');
+}
+const b = document.querySelectorAll('.yearly-amt')
+console.log(b.length);
+for (let i = 0; i < a.length; i++) {
+    b[i].classList.remove('hide-plan');
+}
+// document.querySelectorAll('.yearly-amt').classList.remove('hide-plan');
+ */
+/* 'use strict';
+const ischecked = document.querySelector('#large-storage-checkbox');
+console.log(ischecked);
+/* if(ischecked.checked) {
+    console.log(ischecked);
+} */
+// console.log(ischecked);
+/* ischecked.forEach(box => {
+    console.log(box.value);
+}) */
+
+/*     document.querySelector('#large-storage-checkbox').addEventListener('click' , function () {
+        if(ischecked.checked) {
+    console.log(ischecked.value);
+    console.log(document.querySelector('.hiden-yr-amt').textContent);
+}
+    }) */ 
